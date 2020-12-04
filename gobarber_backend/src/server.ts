96080342e7115b3bd1f9601cc11b05ee8dcc1f4d
Matheus de Paula <matheusdev20@gmail.com>
2020-12-04 00:56:09 -0300

    Adding ReadMe to repository

--- gobarber_backend/src/server.ts
+++ gobarber_backend/src/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes/index';
 import './database/index';
@@ -7,7 +8,7 @@ import uploadConfig from './config/upload';
 import AppError from './errors/AppError';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
@@ -18,7 +19,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
             message: err.message,
         });
     }
-    console.error(err);
     return response.status(500).json({
         status: 'error',
         message: 'Internal Server Error',
