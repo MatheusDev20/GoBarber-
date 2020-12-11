00a7dec38e05459dc35bf7802e392c99e01dbb4e
Matheus de Paula <matheusdev20@gmail.com>
2020-12-11 00:00:20 -0300

    Adding Repository Interface

--- gobarber_backend/src/modules/Appointments/infra/http/appointments.routes.ts
+++ gobarber_backend/src/modules/Appointments/infra/http/appointments.routes.ts
@@ -1,23 +1,23 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 import ensureAuth from '@modules/Users/infra/middlewares/ensureAuth';
-import AppointmentsRepository from '@modules/Appointments/repositories/AppointmentRepository';
+import AppointmentsRepository from '@modules/Appointments/infra/typeorm/repositories/AppointmentRepository';
 import CreateAppointmentService from '@modules/Appointments/services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-
+const appointmentRepository = new AppointmentsRepository();
 appointmentsRouter.use(ensureAuth);
-appointmentsRouter.get('/', async (request, response) => {
-    const appointmentRepository = getCustomRepository(AppointmentsRepository);
+/* appointmentsRouter.get('/', async (request, response) => {
     const appointments = await appointmentRepository.find();
 
     return response.json(appointments);
-});
+}); */
 appointmentsRouter.post('/', async (request, response) => {
     const { provider_id, date } = request.body;
     const parsedDate = parseISO(date);
-    const createAppointment = new CreateAppointmentService();
+    const createAppointment = new CreateAppointmentService(
+        appointmentRepository,
+    );
     const appointment = await createAppointment.execute({
         date: parsedDate,
         provider_id,
