e7072eaa560f3ff4c154e3b5861fedbd30a00ce6
Matheus de Paula <matheus.silva@smartnx.io>
2020-12-30 10:35:57 -0300

    Profile Controller and Service

--- gobarber_backend/src/shared/infra/http/routes/index.ts
+++ gobarber_backend/src/shared/infra/http/routes/index.ts
@@ -3,11 +3,14 @@ import usersRouter from '@modules/Users/infra/http/routes/users.routes';
 import appointmentsRouter from '@modules/Appointments/infra/http/appointments.routes';
 import sessionsRouter from '@modules/Users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/Users/infra/http/routes/password.routes';
+import profileROuter from '@modules/Users/infra/http/routes/profile.route';
 
 const routes = Router();
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileROuter)
+
 
 export default routes;
